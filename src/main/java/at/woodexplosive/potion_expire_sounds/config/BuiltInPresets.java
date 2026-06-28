e5054fa415edc63949cb53061b59859185bcbfeb
WoodEXPLOSIVE <explosive.gamer2000@gmail.com>
2026-06-28 17:25:38 +0200

    Added PVP Preset

--- src/main/java/at/woodexplosive/potion_expire_sounds/config/BuiltInPresets.java
+++ src/main/java/at/woodexplosive/potion_expire_sounds/config/BuiltInPresets.java
@@ -1,5 +1,7 @@
 package at.woodexplosive.potion_expire_sounds.config;
 
+import at.woodexplosive.potion_expire_sounds.PotionExpireSounds;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -9,5 +11,23 @@ public class BuiltInPresets {
 
     static {
         ALL.put("Default", new ModConfig());
+        ALL.put("PVP", pvp());
+    }
+
+    private static ModConfig pvp() {
+        ModConfig c = new ModConfig();
+
+        c.warningThreshold = 200;
+        c.playWarningSound2 = false;
+        c.pitchWarning = 0.5f;
+        c.pitchExpire = 1.3f;
+        c.listType = ModConfig.ListType.WHITELIST;
+        c.effectMap = Map.of(
+                "effect.minecraft.strength", true,
+                "effect.minecraft.speed", true,
+                "effect.minecraft.fire_resistance", true
+        );
+
+        return c;
     }
 }
