ae4acfc60ea26be9e62347e96181bae9bd1626d3
WoodEXPLOSIVE <explosive.gamer2000@gmail.com>
2026-07-20 14:08:56 +0200

    Fixed EditScreen darkening bug

--- src/main/java/at/woodexplosive/potion_expire_sounds/screen/PotionHudEditScreen.java
+++ src/main/java/at/woodexplosive/potion_expire_sounds/screen/PotionHudEditScreen.java
@@ -353,7 +353,7 @@ public class PotionHudEditScreen extends Screen {
         public void extractBackground(@NonNull GuiGraphicsExtractor graphics, int mouseX, int mouseY, float deltaTick) {
             super.extractBackground(graphics, mouseX, mouseY, deltaTick);
             enableScissor(graphics);
-            graphics.fill(this.x, this.y, this.x + this.width, this.y + this.height, 0xC0101010);
+            graphics.fill(this.x, this.y, this.width, this.height, 0xC0101010);
             graphics.disableScissor();
         }
 
